c7c2a14bd1f1950200b55f7486ad71d5d4efd98c
Michael FIG <michael+github@fig.org>
2019-02-13 16:08:56 -0600

    Update Kubernetes.pm

--- lib/Net/Kubernetes.pm
+++ lib/Net/Kubernetes.pm
@@ -1,7 +1,7 @@
 package Net::Kubernetes;
 # ABSTRACT: An object oriented interface to the REST API's provided by kubernetes
 
-our $VERSION = '1.031';
+our $VERSION = '1.032';
 use Moose;
 require Net::Kubernetes::Namespace;
 require LWP::UserAgent;
